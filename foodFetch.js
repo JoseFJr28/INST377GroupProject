bbd57e38882df2ac411fe17c0cf470e97f5e92a9
Jose Fuentes Jr <jfuente7@terpmail.umd.edu>
2023-12-06 15:22:24 -0500

    simple table display

--- foodFetch.js
+++ foodFetch.js
@@ -3,23 +3,42 @@ function findInfo(event) {
 
     //get variables
     var barcode = document.getElementById("barcode").value;
+    var productName = document.getElementById('productName');
+    var theTable = document.getElementById('allergenTable')
 
     //get from api
     //console.log(barcode)
     fetch(`https://world.openfoodfacts.net/api/v2/product/${barcode}`)
-    .then((res) => res.json())
-    .then((info) => {
+        .then((res) => res.json())
+        .then((info) => {
 
-       console.log(info.product)
-        var dataArr = info.product
+           // console.log(info.product)
+            var dataArr = info.product
 
-        for (let x=0; x<dataArr.length;x++){
-            console.log(x)
+          //  console.log(dataArr.allergens_hierarchy)
+            //console.log(dataArr.allergens_hierarchy.length)
+            var allergenCount = dataArr.allergens_hierarchy.length
+            var allergens = dataArr.allergens_hierarchy
+            productName.value = dataArr.product_name
+            //console.log(productName.value)
+
+            let display =
+                `<tr>
+            <th><b>Allergens</b></th>
+            <th><b>Contains?</b></th>
+            </tr>`;
+            
+
+            for (let x = 0; x < allergenCount; x++) {
+                console.log(allergens[x].slice(3))
+
+                display += `<tr>
+            <td id='allergens'>${allergens[x].slice(3)}</td>
+            <td id='allergens'>${productName.value}</td>
+            </tr>`;
+            }
+            theTable.innerHTML += display;
+
         }
-    })
-}
-
-
-function filteredInfo(filter){
-
+        )
 }
