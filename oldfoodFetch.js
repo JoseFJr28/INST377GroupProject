3190c004b875273a3611dba6848d31a691e34850
Climax9 <simon325@terpmail.umd.edu>
2023-12-15 13:15:13 -0500

    merged files, created temp backup

--- oldfoodFetch.js
+++ oldfoodFetch.js
@@ -27,7 +27,7 @@ async function findInfo() {
 
     //The item that is being search
     var dataArr = info.product
-    //console.log(dataArr)
+    console.log(dataArr)
 
     //console.log(allergenCount)
     var allergens = dataArr.allergens_hierarchy
@@ -40,7 +40,6 @@ async function findInfo() {
     //console.log(allergenList)
     return allergenList
 }
-
 /*Returns an array that is selected from the form by the user
 considered unequal array comparison*/
 function pullFromForm() {
@@ -56,10 +55,9 @@ function pullFromForm() {
             }
         }
     })
-    //console.log(selectedAllergens)
+    console.log(selectedAllergens)
     return selectedAllergens
 }
-
 /*Compares both arrays coming from pullfromform and findinfo 
 Array1 == the product allergen hierarchy
 Array2 == the checkbox allergens
@@ -79,7 +77,7 @@ async function filteredInfo() {
         for (let y = 0; y < defaultAllergens.length; y++) {
             //console.log(defaultAllergens[y])
             if (defaultAllergens[y] == selectedAllergens[x]) {
-                //console.log("yes")
+                console.log("yes")
                 triggeredAllergens.push(selectedAllergens[x])
             } else {
                 continue
@@ -90,64 +88,46 @@ async function filteredInfo() {
     return triggeredAllergens
 }
 
-/*Grabs the main array from the comparison to display in table*/
+/*Grabs the main array wfrom the comparison to display in table*/
 async function tableMaker() {
-    
-    //We grab the arrays that ahas the allergens
-    var productAllergens = pullFromForm()//This grabs from the checkbox
-    var editor = document.getElementById("rows") //Grabs the table to create
-    var data = await filteredInfo()//The allergens the users chose and sees if the product has it so if i chose milk and eggs and the product is cheese then I would get both
-
-    //First check if the table has "rows"
+    //counter = 0
+    console.log(document.getElementById("rows").rows.length)
     if(document.getElementById('rows').rows.length == 0){
 
-    //console.log(editor.rows.length)
+    var editor = document.getElementById("rows")
+    var data = await filteredInfo()
+    var defaultAllergens = await findInfo()
+    console.log(editor.rows.length)
 
-    //If their are no allergens, that were chosen, then the message of we are safe.
     if (data.length == 0) {
-        editor.innerHTML += `<tr><td>You are SAFE</td><td>&#128077</td></tr> `
+        editor.innerHTML += `&#128540 You are SAFE&#128077 `
     } else {
-        //If there are allergens chosen; we iterate through the array
+
         for (let x = 0; x < data.length; x++) {
-            //iterate throught the products base allergens
-            for (let y = 0; y < productAllergens.length; y++) {
-                //if one of chosen allergens are in the products allergens
-                if (data[x] == productAllergens[y]) {
-                    console.log(productAllergens[x])
-                    //display is in the table
-                    editor.innerHTML += `<tr><td>${productAllergens[y]}</td><td bgcolor="red" style="color: yellow">Yes</td></tr>`
-                    //immidiately go to the next allergen because we already confirmed its in product
+            for (let y = 0; y < defaultAllergens.length; y++) {
+                if (defaultAllergens[y] == data[x]) {
+                    editor.innerHTML += `<tr><td>${data[x]}</td><td>Yes</td></tr>`
                     x++
-                //If the allergen is not in the product
-                } else if (productAllergens[y] != data[x]){
-                    //we add it to the table but we indicate it is not in the products allergens
-                    editor.innerHTML += `<tr><td>${productAllergens[y]}</td><td bgcolor="green">No</td></tr>`
+                } else {
+                    editor.innerHTML += `<tr><td>${defaultAllergens[x]}</td><td>No</td></tr>`
                 }
             }
         }
     }
     }else{
-        createOrDestroy()
+        document.getElementById("rows").remove()
+        console.log(document.getElementById("rows").rows.length = 0)
+        tableMaker()
     }
+
+    console.log("function is working")
 }
 
-//
 function createOrDestroy(){
-    //The amount of rows in the table
-    var size = document.getElementById('rows').rows.length
-    console.log(document.getElementById('rows').rows.length)
-    //we check if there are any rows 
     if(document.getElementById('rows').rows.length == 0){
-        //if no rows then we make the table
         tableMaker()
     }else{
-        //if there are rows we iterate through that tables row
-        for(let x = 0; x<size; x++){
-            //delete each row
-            document.getElementById('rows').deleteRow(x)
-        }
-        console.log(size)
-        //the create the new table
+        document.getElementById("rows").remove()
         tableMaker()
     }
 }
